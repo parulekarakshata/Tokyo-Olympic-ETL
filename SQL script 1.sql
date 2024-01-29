672d91a4dc90b21abf12c984b625e137c333d51c
parulekarakshata <65888249+parulekarakshata@users.noreply.github.com>
2024-01-28 21:09:11 -0500

    SQL script 1.sql

--- SQL script 1.sql
+++ SQL script 1.sql
@@ -20,3 +20,16 @@ avg(Male) AvgMale
 from entriesgender
 where Discipline = 'Artistic Gymnastics'
 group by Discipline
+
+--Top 10 Countries
+select distinct m.Team_Country, m.Rank_by_Total
+from medals m
+join teams t on m.Team_Country = t.Country
+where Rank_by_Total <= 10
+order by m.Rank_by_Total;
+
+--grouping  athletes by discipline
+select count(discipline), discipline
+from athletes
+group by discipline
+limit 5;
